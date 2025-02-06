34e5a1abed37d324c05e3d938d55cd4bfbc23573
SteveCodeK <awilisteve076@gmail.com>
2025-02-06 13:31:43 +0300

    Delete mysite directory

--- mysite/Mywebsite/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(response):
-    return render(response, 'home.html', {})
