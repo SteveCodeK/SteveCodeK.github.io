5567dd14696d88362cd5d2b9a605befc2e055bb2
SteveCodeK <awilisteve076@gmail.com>
2025-02-06 13:30:14 +0300

    Add files via upload

---
+++ mysite/Mywebsite/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.static import static
+from django.urls import path
+
+from mysite import settings
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
